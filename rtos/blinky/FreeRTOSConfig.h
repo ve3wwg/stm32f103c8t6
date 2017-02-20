e58f1f85791ab83fdd6f9b058018f78f5fb7eac3
Warren Gay <ve3wwg@gmail.com>
2017-02-20 09:21:57 -0500

    Fix pesky DOS newlines

--- rtos/blinky/FreeRTOSConfig.h
+++ rtos/blinky/FreeRTOSConfig.h
@@ -88,7 +88,7 @@
 #define configUSE_PREEMPTION		1
 #define configUSE_IDLE_HOOK			0
 #define configUSE_TICK_HOOK			0
-#define configCPU_CLOCK_HZ			( ( unsigned long ) 72000000 )	
+#define configCPU_CLOCK_HZ			( ( unsigned long ) 36000000 )	
 #define configTICK_RATE_HZ			( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES		( 5 )
 #define configMINIMAL_STACK_SIZE	( ( unsigned short ) 128 )
