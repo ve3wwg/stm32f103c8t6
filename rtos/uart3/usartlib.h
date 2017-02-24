ad0f286a0baa97e06874dc900a7c9a75890f6f5e
Warren Gay <ve3wwg@gmail.com>
2017-02-23 23:09:14 -0500

    put_usart() now putc_usart()

--- rtos/uart3/usartlib.h
+++ rtos/uart3/usartlib.h
@@ -7,12 +7,12 @@
 int open_usart(uint32_t usart,uint32_t baud,const char *cfg,const char *mode,int rts,int cts);
 void close_usart(uint32_t usart);
 
-int put_usart_nb(uint32_t usart,char ch);	/* non-blocking */
-void put_usart(uint32_t usart,char ch);		/* blocking */
+int put_usart_nb(uint32_t usart,char ch);			/* non-blocking */
+void putc_usart(uint32_t usart,char ch);			/* blocking */
 void write_usart(uint32_t usart,const char *buf,uint32_t size); /* blocking */
-void puts_usart(uint32_t usart,const char *buf); /* blocking */
-int getc_usart_nb(uint32_t usart);		/* non-blocking */
-char getc_usart(uint32_t usart);		/* blocking */
+void puts_usart(uint32_t usart,const char *buf);		/* blocking */
+int getc_usart_nb(uint32_t usart);				/* non-blocking */
+char getc_usart(uint32_t usart);				/* blocking */
 
 #endif // UARTLIB_H
 
