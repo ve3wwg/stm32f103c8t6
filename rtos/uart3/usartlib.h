901e6414e8253c2e49a9e48a9a2edc20d324cf87
Warren Gay <ve3wwg@gmail.com>
2017-02-24 23:08:49 -0500

    Notes

--- rtos/uart3/usartlib.h
+++ rtos/uart3/usartlib.h
@@ -1,5 +1,19 @@
 /* usartlib.h -- Interrupt driven USART 
  * Date: Tue Feb 21 20:44:52 2017   (C) Warren Gay ve3wwg
+ *
+ * NOTES:
+ *	(1) It is assumed that the caller has configured the participating GPIO
+ *          lines for the USART, as well as the GPIO RCC.
+ *
+ *	    For example, for USART1:
+ *		GPIOA, A9 is Output (TX)
+ *		GPIOA, A10 is Input (RX)
+ *		GPIOA, A11 is Output (RTS, when used)
+ *		GPIOA, A12 is Input (CTS, when used)
+ *
+ *	(2) open_usart() will start the peripheral RCC.
+ *	(3) open_usart() enables rx interrupts, when required.
+ *
  */
 #ifndef UARTLIB_H
 #define UARTLIB_H
