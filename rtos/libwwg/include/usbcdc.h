5138cc8c5cd4b8f80e6327a7fd7ad9b0b330fc6a
Warren Gay <ve3wwg@gmail.com>
2017-03-15 22:36:01 -0400

    Initial

---
+++ rtos/libwwg/include/usbcdc.h
@@ -0,0 +1,22 @@
+/* libusbcdc header
+ * Warren W. Gay VE3WWG
+ */
+#ifndef LIBUSBCDC_H
+#define LIBUSBCDC_H
+
+#include <stdlib.h>
+#include <string.h>
+
+extern void usb_start(bool gpio_init);
+extern int usb_ready(void);
+
+extern void usb_putch(char ch);
+extern void usb_puts(const char *buf);
+
+extern int usb_getch(void);
+extern int usb_peek(void);
+extern int usb_gets(char *buf,unsigned maxbuf);
+
+#endif /* LIBUSBCDC_H */
+
+/* End libusbcdc.h */
