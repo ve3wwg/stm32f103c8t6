02276d8e4679ca9ffbebf0841905fb34ca68a0f6
Warren Gay <ve3wwg@gmail.com>
2017-03-23 23:11:25 -0400

    In development-- writes now work.

--- rtos/usbcust/posix/test1.cpp
+++ rtos/usbcust/posix/test1.cpp
@@ -29,16 +29,18 @@
 int
 main(int argc,char **argv) {
 	struct usb_bus *bus;
-	struct usb_device *dev, *vusb_dev = 0;
-	bool dev_found = false;		// True if enumeration discovered the device
+	struct usb_device *dev, *usb_dev = 0;
+	int rc;
 
 	usb_init();             	// initialize libusb
+usb_set_debug(7);
 	usb_find_busses();
 	usb_find_devices();
 
-	for ( bus=usb_get_busses(); bus; bus=bus->next ) {
-		for( dev=bus->devices; dev; dev=dev->next ) {
+	for ( bus=usb_get_busses(); bus && !usb_dev; bus=bus->next ) {
+		for( dev=bus->devices; dev && !usb_dev; dev=dev->next ) {
 			struct usb_device_descriptor& desc = dev->descriptor;
+#if 0
 			usb_dev_handle *dh = usb_open(dev);
 			char buf[50];
 
@@ -55,19 +57,20 @@ main(int argc,char **argv) {
 				desc.idProduct,
 				desc.iManufacturer,
 				buf);
-
+#endif
 			if ( desc.idVendor == VEND_ID && desc.idProduct == PROD_ID ) {
+#if 0
 				printf("  Device %s is the test USB device!\n",
 					dev->filename);
-				vusb_dev = dev;
-				dev_found = true;
+#endif
+				usb_dev = dev;
 			}
 		}
 	}
 
-	if ( !dev_found ) {
+	if ( !usb_dev ) {
 		// Bail out if the device was not found:
-		fprintf(stderr,"Test device was not found.\n");
+		fprintf(stderr,"USB device was not found.\n");
 		return 1;
 	}
 
@@ -75,17 +78,24 @@ main(int argc,char **argv) {
 	// Open and communicate with the device
 	//////////////////////////////////////////////////////////////
 
-	usb_dev_handle *handle = usb_open(vusb_dev);
+	usb_dev_handle *handle = usb_open(usb_dev);
 
 	if ( !handle ) {
 		fprintf(stderr,"Unable to open device.\n");
 		return 4;
 	}
 
-	printf("Test Device opened..\n");
+	printf("USB Device opened..\n");
 
-	usb_set_configuration(handle,0);		// Using endpoint 0
-	usb_claim_interface(handle,0);			// Claim it
+	rc = usb_set_configuration(handle,1);
+	if ( rc )
+		printf("%s: rc=%d, usb_set_configuration(1)\n",strerror(-rc),rc);
+
+	rc = usb_claim_interface(handle,0);			// Claim it
+	if ( rc ) printf("%s: rc=%d, usb_claim_interface(0)\n",strerror(-rc),rc);
+
+	rc = usb_set_altinterface(handle,0);
+	if ( rc ) printf("%s: rc=%d, usb_set_altinterface(0)\n",strerror(-rc),rc);
 
 	//////////////////////////////////////////////////////////////
 	// wValue and wIndex can be any application specific data,
@@ -106,9 +116,58 @@ main(int argc,char **argv) {
 		1000			// timeout ms
 	);
 
-	printf("Sent %d bytes: LED should be OFF.\n",txbytes);
+	printf("Sent %d bytes: LED should be ON.\n",txbytes);
 
 	sleep(2);
+
+	//////////////////////////////////////////////////////////////
+	// Test bulk endpoints, using simple synchronous calls:
+	//////////////////////////////////////////////////////////////
+
+	puts("Sending three bulk messages to be echoed back..");
+
+	static const char *msg[] = { "Message One", "Message Two.", "Message Three" };
+	const char *p;
+	char rxbuf[65];
+	int sent;
+
+	for ( int x=0; x<3; ++x ) {
+		for ( p = msg[x]; p && *p; ) {
+			printf("usb_bulk_write()..\n");
+			rc = usb_bulk_write(
+				handle,
+				0x01,
+				p,
+				strlen(p),
+				10000);
+			if ( rc >= 0 ) {
+				p += rc;
+			} else	{
+				printf("%s: rc = %d, usb_bulk_write()\n",strerror(-rc),rc);
+				exit(1);
+			}
+		}
+		printf("#%d sent: '%s'\n",x,msg[x]);
+
+		// Receive message back:
+
+		for ( sent = strlen(msg[x]); sent > 0; ) {
+			rc = usb_bulk_read(
+				handle,
+				0x82,			// Endpoint IN
+				rxbuf,
+				64,
+				10000);
+			if ( rc >= 0 ) {
+				rxbuf[rc] = 0;
+				printf("Recv '%s' (%d)\n",rxbuf,rc);
+				sent -= rc;
+			} else	{
+				printf("%s: rc = %d, usb_bulk_read()\n",strerror(-rc),rc);
+				exit(2);
+			}			
+		}
+	}
 
 	//////////////////////////////////////////////////////////////
 	// Send wValue = 1 to turn on the remote LED
@@ -125,7 +184,7 @@ main(int argc,char **argv) {
 		1000			// timeout ms
 	);
 
-	printf("Sent %d bytes: LED should be ON again.\n",txbytes);
+	printf("Sent %d bytes: LED should be OFF again.\n",txbytes);
 
 	//////////////////////////////////////////////////////////////
 	// Release the Text device
