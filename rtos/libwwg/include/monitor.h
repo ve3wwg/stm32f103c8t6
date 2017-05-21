4b81ab9df5f5088bdd52e5be7080ec192d51daf2
Warren Gay <ve3wwg@gmail.com>
2017-05-21 18:52:59 -0400

    Added monitor.o

---
+++ rtos/libwwg/include/monitor.h
@@ -0,0 +1,12 @@
+/* monitor.h : Monitor routines
+ * Warren W. Gay VE3WWG
+ * Sun May 21 18:50:37 2017
+ */
+#ifndef MONITOR_H
+#define MONITOR_H
+
+void monitor(void);
+
+#endif // MONITOR_H
+
+// End monitor.h
