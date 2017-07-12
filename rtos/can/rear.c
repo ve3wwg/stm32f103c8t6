c85d51d6aca0e750de1dd49c5fd3a6fd25f9056c
Warren Gay <ve3wwg@gmail.com>
2017-07-11 23:18:36 -0400

    Now receives temperature requests and sends it

--- rtos/can/rear.c
+++ rtos/can/rear.c
@@ -10,6 +10,7 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/timer.h>
+#include <libopencm3/stm32/adc.h>
 
 #include "FreeRTOS.h"
 #include "mcuio.h"
@@ -75,6 +76,32 @@ lamp_enable(enum MsgID id,bool enable) {
 }
 
 /*********************************************************************
+ * Read ADC Channel
+ *********************************************************************/
+static uint16_t
+read_adc(uint8_t channel) {
+
+	adc_set_sample_time(ADC1,channel,ADC_SMPR_SMP_1DOT5CYC);
+	adc_start_conversion_direct(ADC1);
+	while ( !adc_eoc(ADC1) )
+		taskYIELD();
+	return adc_read_regular(ADC1);
+}
+
+/*********************************************************************
+ * Return temperature in C * 100
+ *********************************************************************/
+static int
+degrees_C100(void) {
+	int vref, vtemp;
+
+	vtemp = (int)read_adc(ADC_CHANNEL_TEMP) * 3300 / 4095;
+	vref = (int)read_adc(ADC_CHANNEL_VREF) * 3300 / 4095;
+
+	return (vref - vtemp) / 45 + 2500; // temp = (1.43 - Vtemp) / 4.5 + 25.00
+}
+
+/*********************************************************************
  * CAN Receive Callback
  *********************************************************************/
 void
@@ -82,10 +109,12 @@ can_recv(struct s_canmsg *msg) {
 	union u_msg {
 		struct s_lamp_en	lamp;
 	} *msgp = (union u_msg *)msg->data;
+	struct s_temp100 temp_msg;
 
 	gpio_toggle(GPIO_PORT_LED,GPIO_LED);
 
 	if ( !msg->rtrf ) {
+		// Received commands:
 		switch ( msg->msgid ) {
 		case ID_LeftEn:
 		case ID_RightEn:
@@ -97,12 +126,23 @@ can_recv(struct s_canmsg *msg) {
 		default:
 			break;
 		}
+	} else	{
+		// Requests:
+		switch ( msg->msgid ) {
+		case ID_Temp:
+			temp_msg.celciusx100 = degrees_C100();
+			can_xmit(ID_Temp,false,false,sizeof temp_msg,&temp_msg);
+			break;
+		default:
+			break;
+		}
 	}
 }
 
-/*
+/*********************************************************************
  * Monitor task:
- */
+ *********************************************************************/
+
 static void
 controller_task(void *arg __attribute((unused))) {
 
@@ -113,18 +153,18 @@ controller_task(void *arg __attribute((unused))) {
         }
 }
 
-/*
+/*********************************************************************
  * Main program: Device initialization etc.
- */
+ *********************************************************************/
 int
 main(void) {
 
         rcc_clock_setup_in_hse_8mhz_out_72mhz();        // Use this for "blue pill"
 
-	// LED setup
         rcc_periph_clock_enable(RCC_GPIOA);
         rcc_periph_clock_enable(RCC_GPIOB);
         rcc_periph_clock_enable(RCC_GPIOC);
+
 	gpio_set_mode(LAMP_PORT,GPIO_MODE_OUTPUT_2_MHZ,GPIO_CNF_OUTPUT_PUSHPULL,LAMP_LEFT);
 	gpio_set_mode(LAMP_PORT,GPIO_MODE_OUTPUT_2_MHZ,GPIO_CNF_OUTPUT_PUSHPULL,LAMP_RIGHT);
 	gpio_set_mode(LAMP_PORT,GPIO_MODE_OUTPUT_2_MHZ,GPIO_CNF_OUTPUT_PUSHPULL,LAMP_PARK);
@@ -140,6 +180,23 @@ main(void) {
 	initialize_can(false,true,false);		// !nart, locked, altcfg=false PA11/PA12
 
 	xTaskCreate(controller_task,"front",300,NULL,configMAX_PRIORITIES-1,NULL);
+
+	rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_ADC1EN);
+	adc_power_off(ADC1);
+	rcc_peripheral_reset(&RCC_APB2RSTR, RCC_APB2RSTR_ADC1RST);
+	rcc_peripheral_clear_reset(&RCC_APB2RSTR, RCC_APB2RSTR_ADC1RST);
+	rcc_set_adcpre(RCC_CFGR_ADCPRE_PCLK2_DIV2);
+	adc_set_dual_mode(ADC_CR1_DUALMOD_IND);
+	adc_disable_scan_mode(ADC1);
+	adc_set_right_aligned(ADC1);
+	adc_set_single_conversion_mode(ADC1);
+	adc_set_sample_time(ADC1,ADC_CHANNEL_TEMP,ADC_SMPR_SMP_1DOT5CYC);
+	adc_enable_temperature_sensor();
+	adc_power_on(ADC1);
+	adc_reset_calibration(ADC1);
+	adc_calibrate_async(ADC1);
+	while ( adc_is_calibrating(ADC1) );
+
 	vTaskStartScheduler();
 	for (;;);
 }
