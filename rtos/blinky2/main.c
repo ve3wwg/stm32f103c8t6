dcb6983c0fe639668128cc0aadc1b489fbd9cdf7
Warren Gay <ve3wwg@gmail.com>
2017-08-05 11:19:59 -0400

    Cleanup

--- rtos/blinky2/main.c
+++ rtos/blinky2/main.c
@@ -9,23 +9,23 @@
 
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
-#include <libopencm3/cm3/nvic.h>
 
-#define mainECHO_TASK_PRIORITY				( tskIDLE_PRIORITY + 1 )
+#define mainECHO_TASK_PRIORITY ( tskIDLE_PRIORITY + 1 )
 
-extern void vApplicationStackOverflowHook(xTaskHandle *pxTask,signed portCHAR *pcTaskName);
+extern void vApplicationStackOverflowHook(
+	xTaskHandle *pxTask,
+	signed portCHAR *pcTaskName);
 
 void
-vApplicationStackOverflowHook(xTaskHandle *pxTask,signed portCHAR *pcTaskName) {
-	(void)pxTask;
-	(void)pcTaskName;
-	for(;;);
+vApplicationStackOverflowHook(
+  xTaskHandle *pxTask __attribute((unused)),
+  signed portCHAR *pcTaskName __attribute((unused))
+) {
+	for(;;);	// Loop forever here..
 }
 
 static void
-task1(void *args) {
-
-	(void)args;
+task1(void *args __attribute((unused))) {
 
 	for (;;) {
 		gpio_toggle(GPIOC,GPIO13);
@@ -37,11 +37,13 @@ int
 main(void) {
 
 	rcc_clock_setup_in_hse_8mhz_out_72mhz();	// Use this for "blue pill"
+
 	rcc_periph_clock_enable(RCC_GPIOC);
 	gpio_set_mode(GPIOC,GPIO_MODE_OUTPUT_2_MHZ,GPIO_CNF_OUTPUT_PUSHPULL,GPIO13);
 
 	xTaskCreate(task1,"LED",100,NULL,configMAX_PRIORITIES-1,NULL);
 	vTaskStartScheduler();
+
 	for (;;);
 	return 0;
 }
