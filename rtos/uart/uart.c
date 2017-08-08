65aa5643282bec923fad719cb6a9040efe3c44b8
Warren Gay <ve3wwg@gmail.com>
2017-08-08 14:39:45 -0400

    Cleanup for publication

--- rtos/uart/uart.c
+++ rtos/uart/uart.c
@@ -85,7 +85,11 @@ main(void) {
 
 	// PC13:
 	rcc_periph_clock_enable(RCC_GPIOC);
-	gpio_set_mode(GPIOC,GPIO_MODE_OUTPUT_2_MHZ,GPIO_CNF_OUTPUT_PUSHPULL,GPIO13);
+	gpio_set_mode(
+		GPIOC,
+                GPIO_MODE_OUTPUT_2_MHZ,
+                GPIO_CNF_OUTPUT_PUSHPULL,
+                GPIO13);
 
 	uart_setup();
 
