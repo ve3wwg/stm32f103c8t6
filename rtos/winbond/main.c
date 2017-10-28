70e02874679ffec67d3182d1f5fd989a89e5f140
Warren Gay <ve3wwg@gmail.com>
2017-10-27 23:50:35 -0400

    Initial

---
+++ rtos/winbond/main.c
@@ -0,0 +1,459 @@
+/* Simple LED task demo:
+ *
+ * The LED on PC13 is toggled in task1.
+ */
+#include <string.h>
+#include <ctype.h>
+
+#include "mcuio.h"
+#include "miniprintf.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/spi.h>
+
+#define W25_CMD_MANUF_DEVICE	0x90
+#define W25_CMD_JEDEC_ID	0x9F
+#define W25_CMD_WRITE_EN	0x06
+#define W25_CMD_WRITE_DI	0x04
+#define W25_CMD_READ_SR1	0x05
+#define W25_CMD_READ_SR2	0x35
+#define W25_CMD_CHIP_ERASE	0xC7
+#define W25_CMD_READ_DATA	0x03
+#define W25_CMD_WRITE_DATA	0x02
+#define W25_CMD_READ_UID	0x4B
+#define W25_CMD_PWR_ON		0xAB
+#define W25_CMD_PWR_OFF		0xB9
+
+#define DUMMY			0x00
+
+#define W25_SR1_BUSY		0x01
+#define W25_SR1_WEL		0x02
+
+static const char *cap[3] = {
+	"W25X16",	// 14
+	"W25X32",	// 15
+	"W25X64"	// 16
+};	
+
+static uint8_t
+w25_read_sr1(uint32_t spi) {
+	uint8_t sr1;
+
+	spi_enable(spi);
+	spi_xfer(spi,W25_CMD_READ_SR1);
+	sr1 = spi_xfer(spi,0x00);
+	spi_disable(spi);
+	return sr1;
+}
+
+static uint8_t
+w25_read_sr2(uint32_t spi) {
+	uint8_t sr1;
+
+	spi_enable(spi);
+	spi_xfer(spi,W25_CMD_READ_SR2);
+	sr1 = spi_xfer(spi,0x00);
+	spi_disable(spi);
+	return sr1;
+}
+
+static void
+w25_wait(uint32_t spi) {
+
+	while ( w25_read_sr1(spi) & W25_SR1_BUSY )
+		taskYIELD();
+}
+
+static void
+w25_write_en(uint32_t spi,bool en) {
+
+	w25_wait(spi);
+
+	spi_enable(spi);
+	spi_xfer(spi,en ? W25_CMD_WRITE_EN : W25_CMD_WRITE_DI);
+	spi_disable(spi);
+}
+
+static uint16_t
+w25_manuf_device(uint32_t spi) {
+	uint16_t info;
+
+	w25_wait(spi);
+	spi_enable(spi);
+	spi_xfer(spi,W25_CMD_MANUF_DEVICE);	// Byte 1
+	spi_xfer(spi,DUMMY);			// Dummy1 (2)
+	spi_xfer(spi,DUMMY);			// Dummy2 (3)
+	spi_xfer(spi,0x00);			// Byte 4
+	info = spi_xfer(spi,DUMMY) << 8;	// Byte 5
+	info |= spi_xfer(spi,DUMMY);		// Byte 6
+	spi_disable(spi);
+	return info;
+}
+
+static uint32_t
+w25_JEDEC_ID(uint32_t spi) {
+	uint32_t info;
+
+	w25_wait(spi);
+	spi_enable(spi);
+	spi_xfer(spi,W25_CMD_JEDEC_ID);
+	info = spi_xfer(spi,DUMMY);		 // Manuf.
+	info = (info << 8) | spi_xfer(spi,DUMMY);// Memory Type
+	info = (info << 8) | spi_xfer(spi,DUMMY);// Capacity
+	spi_disable(spi);
+
+	return info;
+}
+
+static void
+w25_read_uid(uint32_t spi,void *buf,uint16_t bytes) {
+	uint8_t *udata = (uint8_t*)buf;
+
+	if ( bytes > 8 )
+		bytes = 8;
+	else if ( bytes <= 0 )
+		return;
+
+	w25_wait(spi);
+	spi_enable(spi);
+	spi_xfer(spi,W25_CMD_READ_UID);
+	for ( uint8_t ux=0; ux<4; ++ux )
+		spi_xfer(spi,DUMMY);
+	for ( uint8_t ux=0; ux<bytes; ++ux )
+		udata[ux] = spi_xfer(spi,DUMMY);
+	spi_disable(spi);
+}
+
+static void
+w25_power(uint32_t spi,bool on) {
+
+	if ( !on )
+		w25_wait(spi);
+	spi_enable(spi);
+	spi_xfer(spi,on ? W25_CMD_PWR_ON : W25_CMD_PWR_OFF);
+	spi_disable(spi);
+}
+
+static bool
+w25_chip_erase(uint32_t spi) {
+	uint8_t sr1;
+
+	w25_wait(spi);
+	sr1 = w25_read_sr1(spi);
+	if ( !(sr1 & W25_SR1_WEL) ) {
+		std_printf("Not Erased! Chip is not write enabled.\n");
+		return false;
+	}
+
+	w25_write_en(spi,true);
+	spi_enable(spi);
+	spi_xfer(spi,W25_CMD_CHIP_ERASE);
+	spi_disable(spi);
+
+	std_printf("Erasing chip..\n");
+
+	w25_wait(spi);
+	sr1 = w25_read_sr1(spi);
+	if ( sr1 & W25_SR1_WEL ) {
+		std_printf("Not Erased! Chip erase failed.\n");
+		return false;
+	}
+
+	std_printf("Chip erased!\n");
+	return true;
+}
+
+static uint32_t		// New address is returned
+w25_read_data(uint32_t spi,uint32_t addr,void *data,uint32_t bytes) {
+	uint8_t *udata = (uint8_t*)data;
+
+	w25_wait(spi);
+
+	spi_enable(spi);
+	spi_xfer(spi,W25_CMD_READ_DATA);
+	spi_xfer(spi,addr >> 16);
+	spi_xfer(spi,(addr >> 8) & 0xFF);
+	spi_xfer(spi,addr & 0xFF);
+
+	for ( ; bytes-- > 0; ++addr )
+		*udata++ = spi_xfer(spi,0x00);
+
+	spi_disable(spi);
+	return addr;	
+}
+
+static uint32_t		// New address is returned
+w25_write_data(uint32_t spi,uint32_t addr,void *data,uint32_t bytes) {
+	uint8_t *udata = (uint8_t*)data;
+
+	if ( !(w25_read_sr1(spi) & W25_SR1_WEL) ) {
+		std_printf("Write disabled: Cannot write byte.\n");
+		return addr;
+	}
+
+	w25_wait(spi);
+
+	spi_enable(spi);
+	spi_xfer(spi,W25_CMD_WRITE_DATA);
+	spi_xfer(spi,addr >> 16);
+	spi_xfer(spi,(addr >> 8) & 0xFF);
+	spi_xfer(spi,addr & 0xFF);
+
+	for ( ; bytes-- > 0; ++addr )
+		spi_xfer(spi,*udata++);
+
+	spi_disable(spi);
+	return addr;	
+}
+
+static void
+flash_status(void) {
+
+	std_printf("SR1 = %02X\n",w25_read_sr1(SPI1));
+	std_printf("SR2 = %02X\n",w25_read_sr2(SPI1));
+}
+
+static unsigned
+get_data24(const char *prompt) {
+	unsigned v = 0u, count = 0u;
+	char ch;
+
+	std_printf("%s: ",prompt);
+
+	while ( (ch = std_getc()) != '\r' ) {
+		if ( ch >= '0' && ch <= '9' ) {
+			v <<= 4;
+			v |= ch & 0x0F;
+			std_putc(ch);
+		} else 	{
+			if ( isalpha(ch) )
+				ch = toupper(ch);
+			if ( ch >= 'A' && ch <= 'F' ) {
+				v <<= 4;
+				v |= ((ch & 0x0F) - 1 + 10);
+				std_putc(ch);
+			} else	break;
+		}
+		if ( ++count >= 6 )
+			break;
+	}
+	return v & 0xFFFFFF;
+}
+
+static unsigned
+get_data8(const char *prompt) {
+	unsigned v = 0u, count = 0u;
+	char ch;
+
+	std_printf("%s: ",prompt);
+
+	while ( (ch = std_getc()) != '\r' ) {
+		if ( ch == '"' || ch == '\'' ) {
+			v = std_getc();
+			break;
+		}
+		if ( ch >= '0' && ch <= '9' ) {
+			v <<= 4;
+			v |= ch & 0x0F;
+			std_putc(ch);
+		} else 	{
+			if ( isalpha(ch) )
+				ch = toupper(ch);
+			if ( ch >= 'A' && ch <= 'F' ) {
+				v <<= 4;
+				v |= ((ch & 0x0F) - 1 + 10);
+				std_putc(ch);
+			} else	break;
+		}
+		if ( ++count >= 2 )
+			break;
+	}
+	return v & 0xFF;
+}
+
+/*
+ * Monitor task:
+ */
+static void
+monitor_task(void *arg __attribute((unused))) {
+	int ch, wel = 0, devx;
+	unsigned addr = 0u;
+	uint8_t data = 0, idbuf[8];
+	uint32_t info;
+	const char *device;
+	bool menuf = true;
+	
+	for (;;) {
+		if ( menuf ) {
+			std_printf(
+				"\nWinbond Menu:\n"
+				"  0 ... Power down\n"
+				"  1 ... Power on\n"
+				"  a ... Set address\n"
+				"  d ... Set data\n"
+				"  e ... Chip erase\n"
+				"  i ... Manufacture/Device info\n"
+				"  j ... JEDEC ID info\n"
+				"  r ... Read byte\n"
+				"  p ... Program byte\n"
+				"  s ... Flash status\n"
+				"  u ... Read unique ID\n"
+				"  w ... Toggle Write Enable Latch\n"
+			);
+			std_printf("Address: %012X\n",addr);
+			std_printf("Data:    %02X\n",data);
+		}
+		menuf = false;
+
+		std_printf("\n: ");
+		ch = std_getc();
+
+		if ( isalpha(ch) )
+			ch = toupper(ch);
+		std_printf("%c\n",ch);
+
+		switch ( ch ) {
+		case '?':
+		case '\r':
+		case '\n':
+			menuf = true;
+			break;		
+		case '0':
+			w25_power(SPI1,0);
+			break;
+		case '1':
+			w25_power(SPI1,1);
+			break;
+		case 'I':
+			info = w25_manuf_device(SPI1);
+			devx = (int)(info & 0xFF)-0x14;
+			if ( devx < 3 )
+				device = cap[devx];
+			else	device = "unknown";
+			std_printf("Manufacturer $%02X Device $%02X (%s)\n",
+				(uint16_t)info>>8,(uint16_t)info&0xFF,
+				device);
+			break;
+		case 'J':
+			info = w25_JEDEC_ID(SPI1);
+			devx = (int)(info & 0xFF)-0x15;	// Offset is 1 higher here
+			if ( devx < 3 )
+				device = cap[devx];
+			else	device = "unknown";
+			std_printf("Manufacturer $%02X Type $%02X Capacity $%02X (%s)\n",
+				(uint16_t)(info>>16),
+				(uint16_t)((info>>8)&0xFF),
+				(uint16_t)(info&0xFF),
+				device);
+			break;
+		case 'U':
+			w25_read_uid(SPI1,idbuf,sizeof idbuf);
+			std_printf("Unique ID: $");
+			for ( unsigned ux=0; ux<sizeof idbuf; ++ux )
+				std_printf("%02X",idbuf[ux]);
+			std_putc('\n');
+			break;
+		case 'S':
+			flash_status();
+			break;
+		case 'W':
+			w25_write_en(SPI1,wel^=1);
+			flash_status();
+			break;
+		case 'E':
+			w25_chip_erase(SPI1);
+			break;
+		case 'A':
+			addr = get_data24("Address");
+			break;
+		case 'D':
+			data = get_data8("Data");
+			break;
+		case 'R':
+			addr = w25_read_data(SPI1,addr,(char*)&data,1);
+			std_printf("$%06X %02X",addr,data);
+			if ( data >= ' ' && data < 0x7F )
+				std_printf(" '%c'\n",data);
+			else	std_putc('\n');
+			break;
+		case 'P':
+			if ( !(w25_read_sr1(SPI1) & W25_SR1_WEL) ) {
+				std_printf("Can't: write protected.\n");
+			} else	{
+				std_printf("$%06X: ",addr);
+				data = get_data8("");
+				addr = w25_write_data(SPI1,addr,&data,1);
+			}
+			break;
+		default:
+			std_printf(" ???\n");
+			menuf = true;
+		}
+	}
+}
+
+static void
+spi_setup(void) {
+
+	rcc_periph_clock_enable(RCC_SPI1);
+	gpio_set_mode(
+		GPIOA,
+                GPIO_MODE_OUTPUT_50_MHZ,
+        	GPIO_CNF_OUTPUT_ALTFN_PUSHPULL,
+                GPIO4|GPIO5|GPIO7		// NSS=PA4,SCK=PA5,MOSI=PA7
+	);
+	gpio_set_mode(
+		GPIOA,
+		GPIO_MODE_INPUT,
+		GPIO_CNF_INPUT_FLOAT,
+		GPIO6				// MISO=PA6
+	);
+	spi_reset(SPI1); 
+	gpio_set(GPIOA,GPIO4);			// Set high
+
+	spi_init_master(
+		SPI1,
+                SPI_CR1_BAUDRATE_FPCLK_DIV_256,
+                SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE,
+		SPI_CR1_CPHA_CLK_TRANSITION_1,
+	        SPI_CR1_DFF_8BIT,
+	        SPI_CR1_MSBFIRST
+	);
+
+	spi_disable_software_slave_management(SPI1);
+	spi_enable_ss_output(SPI1);
+
+	gpio_set(GPIOC,GPIO13);				// PC13 = on
+}
+
+int
+main(void) {
+
+	rcc_clock_setup_in_hse_8mhz_out_72mhz();	// Blue pill
+
+	rcc_periph_clock_enable(RCC_GPIOA);
+	rcc_periph_clock_enable(RCC_GPIOB);
+	rcc_periph_clock_enable(RCC_GPIOC);
+	rcc_periph_clock_enable(RCC_AFIO);
+
+	// LED on PC13
+	gpio_set_mode(GPIOC,GPIO_MODE_OUTPUT_2_MHZ,GPIO_CNF_OUTPUT_PUSHPULL,GPIO13);
+
+	spi_setup();
+
+	usb_start(1);
+	std_set_device(mcu_usb);			// Use USB for std I/O
+	gpio_clear(GPIOC,GPIO13);			// PC13 = off
+
+	xTaskCreate(monitor_task,"monitor",100,NULL,configMAX_PRIORITIES-1,NULL);
+	vTaskStartScheduler();
+	for (;;);
+	return 0;
+}
+
+// End
